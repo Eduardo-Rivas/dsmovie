9707a6b068e882c4370158a25bd311367c2552ff
Eduardo-Rivas <edumeru46@gmail.com>
2022-05-09 21:26:32 -0300

    Creación de Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar()
+{ 
+    return(
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+
+                        <a href="https://github.com/Eduardo-Rivas">
+                            <div className= 'dsmovie-contat-content'>
+                                <GithubIcon />
+                                <p className='dsmovie-contat-link'>/devsuperior</p>
+                            </div>
+                        </a>
+                </div>
+            </nav>
+      </header>    
+    );
+}
+export default Navbar;
